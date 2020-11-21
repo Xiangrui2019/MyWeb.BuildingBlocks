d3a945e68ba15e4cf0cccdea4462626bc0d0f754
HomeServing <xiangrui@aiursoft.com>
2020-11-21 21:58:23 +0800

    testing

--- src/Impls/DBTools.EntityFramework/Extends.cs
+++ src/Impls/DBTools.EntityFramework/Extends.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
