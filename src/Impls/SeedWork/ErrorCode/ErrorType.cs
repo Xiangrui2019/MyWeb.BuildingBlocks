554ab7f3c215dc2fcac921196f15f3d8b032be66
HomeServing <xiangrui@aiursoft.com>
2020-11-21 12:24:16 +0800

    add SeedWork.UnitTests

--- src/Impls/SeedWork/ErrorCode/ErrorType.cs
+++ src/Impls/SeedWork/ErrorCode/ErrorType.cs
@@ -1,9 +1,4 @@
 ﻿using SeedWork.Models.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeedWork.ErrorCode
 {
