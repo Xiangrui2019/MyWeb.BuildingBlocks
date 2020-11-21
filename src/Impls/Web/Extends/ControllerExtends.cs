725a2be3efea0de1060a68698ed200d1db51a951
HomeServing <xiangrui@aiursoft.com>
2020-11-21 20:31:21 +0800

    add easying using controller

---
+++ src/Impls/Web/Extends/ControllerExtends.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeedWork.ErrorCode;
+using SeedWork.Models.Message;
+
+namespace Web.Extends
+{
+    public static class ControllerExtends
+    {
+        public static IActionResult Message(this ControllerBase controller, ErrorType errorType, string errorMessage)
+        {
+            return controller.Ok(new MessageModel
+            {
+                Code = errorType,
+                Message = errorMessage
+            });
+        }
+    }
+}
