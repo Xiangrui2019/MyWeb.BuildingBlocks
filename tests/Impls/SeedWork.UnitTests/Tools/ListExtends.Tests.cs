2c8736404524a2aec9e5226dc14a879fc1d9525a
HomeServing <xiangrui@aiursoft.com>
2020-11-21 13:39:29 +0800

    add ListExtends and ListExtends.Test.cs

---
+++ tests/Impls/SeedWork.UnitTests/Tools/ListExtends.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SeedWork.Tools;
+using Xunit;
+
+namespace SeedWork.UnitTests.Tools
+{
+    public class ListExtends_Tests
+    {
+        [Fact]
+        public void TestShuffle()
+        {
+            var list = new List<int>(){ 1, 2, 3, 4, 5, 6 };
+            var listSrc = list;
+            list.Shuffle();
+            
+            Assert.Equal(list.First(), listSrc.First());
+        }
+    }
+}
