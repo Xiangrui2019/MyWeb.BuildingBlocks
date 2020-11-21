9b9cb3883fc57befe46612361d21c441c9867ce5
HomeServing <xiangrui@aiursoft.com>
2020-11-21 15:52:19 +0800

    add apimodelex

---
+++ src/Impls/Handler/Exceptions/UnexpectedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using SeedWork.ErrorCode;
+using SeedWork.Models.Message;
+
+namespace Handler.Exceptions
+{
+    public class UnexpectedException : Exception
+    {
+        public MessageModel Response { get; set; }
+        public ErrorType Code => Response.Code;
+
+        public UnexpectedException(MessageModel model) : base(model.Message)
+        {
+            Response = Response;
+        }
+    }
+}
---
+++ tests/Impls/Handler.UnitTests/Exceptions/ApiModelException.Tests.cs
@@ -0,0 +1,33 @@
+﻿using Handler.Exceptions;
+using SeedWork.ErrorCode;
+using SeedWork.Services;
+using Xunit;
+
+namespace Handler.UnitTests.Exceptions
+{
+    public class ApiModelException_Tests
+    {
+        [Fact]
+        public void TestBasic()
+        {
+            var exception = new ApiModelException(ErrorType.Success, "Testing");
+            
+            Assert.Equal(1, exception.Code.Id);
+            Assert.Equal("Testing", exception.Message);
+        }
+
+        [Fact]
+        public void TestThrowException()
+        {
+            try
+            {
+                throw new ApiModelException(ErrorType.Success, "Testing");
+            }
+            catch(ApiModelException e)
+            {
+                Assert.Equal(1, e.Code.Id);
+                Assert.Equal("Testing", e.Message);
+            }
+        }
+    }
+}
