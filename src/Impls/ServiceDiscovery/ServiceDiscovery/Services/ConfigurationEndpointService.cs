f281084a8384c1eb463399c008860dd3cf5ff1ac
HomeServing <xiangrui@aiursoft.com>
2020-11-22 21:16:26 +0800

    add endpoitn service

---
+++ src/Impls/ServiceDiscovery/ServiceDiscovery/Services/ConfigurationEndpointService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using ServiceDiscovery.Abstract.Interfaces;
+
+namespace ServiceDiscovery.Services
+{
+    public class ConfigurationEndpointService : IServiceEndpointsGetter
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationEndpointService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public Task<List<string>> GetLocalServiceEndpointsAsync()
+        {
+            var ips = new List<string>();
+            ips.Add(_configuration["ServiceDiscoveryOptions:ServiceEndpoint"]);
+            
+            return Task.FromResult(ips);
+        }
+    }
+}
