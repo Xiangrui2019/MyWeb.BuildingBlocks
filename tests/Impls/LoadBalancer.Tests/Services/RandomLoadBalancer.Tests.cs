b94655fdf8cef79f8aa76105122618c8b9c786c8
HomeServing <xiangrui@aiursoft.com>
2020-11-22 21:55:56 +0800

    add lb

---
+++ tests/Impls/LoadBalancer.Tests/Services/RandomLoadBalancer.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LoadBalancer.Abstract;
+using LoadBalancer.Services;
+using Xunit;
+
+namespace LoadBalancer.Tests.Services
+{
+    public class RandomLoadBalancer_Tests
+    {
+        [Fact]
+        public void TestLB()
+        {
+            ILoadBalancer lb = new RandomLoadBalancer();
+            var list = new List<string>
+            {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I"
+            };
+
+            Assert.NotEqual("A", lb.Endpoint(list));
+            Assert.NotEqual("A", lb.Endpoint(list));
+            Assert.NotEqual("A", lb.Endpoint(list));
+        }
+    }
+}
