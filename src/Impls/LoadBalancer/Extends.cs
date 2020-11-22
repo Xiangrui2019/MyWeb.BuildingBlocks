210e8f8841a9913cd8c79c8f509f6ad1bada305e
HomeServing <xiangrui@aiursoft.com>
2020-11-22 21:51:47 +0800

    add balancer

---
+++ src/Impls/LoadBalancer/Extends.cs
@@ -0,0 +1,16 @@
+﻿using LoadBalancer.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoadBalancer
+{
+    public static class Extends
+    {
+        public static IServiceCollection AddLoadBalancer<TLb>(this IServiceCollection services)
+            where TLb : class, ILoadBalancer
+        {
+            services.AddSingleton<ILoadBalancer, TLb>();
+            
+            return services;
+        }
+    }
+}
---
+++ src/Impls/LoadBalancer/Services/RandomLoadBalancer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LoadBalancer.Abstract;
+
+namespace LoadBalancer.Services
+{
+    public class RandomLoadBalancer : ILoadBalancer
+    {
+        public string Endpoint(List<string> endpoints)
+        {
+            return endpoints.ElementAt(
+                Environment.TickCount % endpoints.Count());
+        }
+    }
+}
