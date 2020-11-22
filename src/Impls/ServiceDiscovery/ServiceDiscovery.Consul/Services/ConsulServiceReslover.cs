ea10dbc90228f91c98a4d04ac446dac57402c6fd
HomeServing <xiangrui@aiursoft.com>
2020-11-22 21:03:40 +0800

    add defaulter

--- src/Impls/ServiceDiscovery/ServiceDiscovery.Consul/Services/ConsulServiceReslover.cs
+++ src/Impls/ServiceDiscovery/ServiceDiscovery.Consul/Services/ConsulServiceReslover.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
 using Consul;
+using Microsoft.Extensions.Logging;
 using ServiceDiscovery.Abstract.Interfaces;
 
 namespace ServiceDiscovery.Consul.Services
@@ -11,20 +12,25 @@ namespace ServiceDiscovery.Consul.Services
     public class ConsulServiceReslover : IServiceReslover, IDisposable
     {
         private readonly IConsulClient _client;
+        private readonly ILogger<ConsulServiceReslover> _logger;
         private Dictionary<string, AgentService> _services;
         private readonly Timer _timer;
 
-        public ConsulServiceReslover(IConsulClient client)
+        public ConsulServiceReslover(
+            IConsulClient client,
+            ILogger<ConsulServiceReslover> logger)
         {
             _client = client;
+            _logger = logger;
             SyncConsul(null, null);
-            _timer = new Timer(10000);
+            _timer = new Timer(5000);
             _timer.Elapsed += new ElapsedEventHandler(SyncConsul);
             _timer.Start();
         }
 
         private void SyncConsul(object source, System.Timers.ElapsedEventArgs e)
         {
+            _logger.LogInformation("Syncing Services from Consul Agent.");
             _services = _client.Agent.Services().Result.Response;
         }
 
---
+++ src/Impls/ServiceDiscovery/ServiceDiscovery/Services/PodIpEndpointService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ServiceDiscovery.Abstract.Interfaces;
+
+namespace ServiceDiscovery.Services
+{
+    public class PodIpEndpointService : IServiceEndpointsGetter
+    {
+        public Task<List<string>> GetLocalServiceEndpointsAsync()
+        {
+            var ips = new List<string>();
+            ips.Add(
+                $"http://{Environment.GetEnvironmentVariable("POD_IP")}:80");
+            
+            return Task.FromResult(ips);
+        }
+    }
+}
