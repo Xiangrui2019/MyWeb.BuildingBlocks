8a0cd097199e0afbb1a6b17c6444f4d81f3efe3a
HomeServing <xiangrui@aiursoft.com>
2020-11-23 22:01:38 +0800

    basic get

--- src/Impls/Resilience.Http/Services/HttpService.cs
+++ src/Impls/Resilience.Http/Services/HttpService.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Resilience.Http.Interfaces;
 using Resilience.Http.Models;
@@ -8,20 +13,39 @@ namespace Resilience.Http.Services
     public class HttpService : IHttpService
     {
         private readonly HttpClient _httpClient;
+        private readonly Regex _regex;
         
         public HttpService(IHttpClientFactory clientFactory)
         {
             _httpClient = clientFactory.CreateClient();
+            _regex = new Regex("^https://", RegexOptions.Compiled);
         }
         
         public string Get(HttpUrl url, bool forceHttp = false)
         {
-            throw new System.NotImplementedException();
+            
         }
 
-        public Task<string> GetAsync(HttpUrl url, bool forceHttp = false)
+        public async Task<string> GetAsync(HttpUrl url, bool forceHttp = false)
         {
-            throw new System.NotImplementedException();
+            if (forceHttp && !url.IsLocalhost())
+            {
+                url.Address = _regex.Replace(url.Address, "http://");
+            }
+
+            var response = await HttpInvokeAsync(
+                HttpMethod.Get,
+                url.ToString(),
+                new FormUrlEncodedContent(new Dictionary<string, string>()));
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new WebException($"The remote server returned unexpected status code: {response.StatusCode} - {response.ReasonPhrase}.");
+            }
         }
 
         public T GetToJson<T>(HttpUrl url, bool forceHttp = false)
@@ -112,6 +136,22 @@ namespace Resilience.Http.Services
         public Task<T> DeleteJsonToJsonAsync<T>(HttpUrl url, HttpUrl postData, bool forceHttp = false)
         {
             throw new System.NotImplementedException();
+        }
+        
+        private async Task<HttpResponseMessage> HttpInvokeAsync(
+            HttpMethod method,
+            string url,
+            HttpContent content)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url.ToString())
+            {
+                Content = content,
+            };
+            request.Headers.Add("accept", "application/json, text/html");
+
+            using var response = await _httpClient.SendAsync(request);
+
+            return response;
         }
     }
 }
